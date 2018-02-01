77364fba63c9b4438eac0ec36b1cbdfbe4dce7b0
mczu <14128860+mczu@users.noreply.github.com>
2018-02-01 23:20:52 +0100

    login and register

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,20 +1,33 @@
-var login = document.getElementById("exampleInputPassword1").value;
-var password = document.getElementById("exampleInputPassword1").value;
-
-function prepareLoginData() {
-    console.log(password);
-}
-
 function createLoginObject() {
+    var login = document.getElementById("exampleInputLogin").value;
+    var password = document.getElementById("exampleInputPassword1").value;
+
     var LoginObject = {};
     LoginObject.login = login;
     LoginObject.password = password;
+    console.log(LoginObject);
     return LoginObject;
 }
 
+var url = 'http://localhost:8080/login';
+
+var newData = JSON.stringify(createLoginObject());
+
+var request = new Request(url, {
+    method: 'POST',
+    body: newData,
+    headers: new Headers({
+        'Content-Type': 'application/json'
+    })
+});
+
 function sendLoginData() {
-    var loginJSON = createLoginObject();
-    console.log(loginJSON);
+    console.log(newData);
+    fetch(request)
+        .then(function (data) {
+            // Handle response we get from the API
+            console.log(data);
+        });
 }
 
 document.getElementById("submitLoginData").addEventListener("click", sendLoginData);
--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,22 +1,36 @@
-var login = document.getElementById("registerInputPassword1").value;
-var password = document.getElementById("registerInputPassword1").value;
-var email = document.getElementById("registerInputEmail").value;
-
-function prepareRegistrationData() {
-    console.log(password);
-}
-
 function createRegistrationObject() {
+    var login = document.getElementById("registerInputPassword1").value;
+    var password = document.getElementById("registerInputPassword1").value;
+    var email = document.getElementById("registerInputEmail1").value;
+
     var RegistrationObject = {};
     RegistrationObject.login = login;
     RegistrationObject.password = password;
     RegistrationObject.email = email;
+    console.log(RegistrationObject);
     return RegistrationObject;
 }
 
+var url = 'http://localhost:8080/users';
+
+var newData = JSON.stringify(createRegistrationObject());
+
+var request = new Request(url, {
+    method: 'POST',
+    body: newData,
+    headers: new Headers({
+        'Content-Type': 'application/json'
+    })
+});
+
 function sendRegistrationData() {
-    var loginJSON = createRegistrationObject();
-    console.log(loginJSON);
+    console.log(newData);
+    fetch(request)
+        .then(function (data) {
+            // Handle response we get from the API
+            console.log(data);
+        });
+    //if ok -> login
 }
 
-document.getElementById("submitLoginData").addEventListener("click", sendRegistrationData);
+document.getElementById("submitRegistrationData").addEventListener("click", sendRegistrationData);
