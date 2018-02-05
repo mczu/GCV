0b9e0da08dfbf392d2eec63fa25a4a4e3332731a
mczu <14128860+mczu@users.noreply.github.com>
2018-02-05 02:38:26 +0100

    put cv

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -4,77 +4,14 @@ console.log(cvUrl);
 var showUrl = "resumes\/" + cvUrl;
 console.log(showUrl);
 
-
-
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    //TEST
- //general data
-  $scope.firstName= "Jan";
-  $scope.lastName= "Kowalski";
-  $scope.email = "jan.kowalski@gmail.com";
-  $scope.phoneNumber = 596690424;
-  $scope.address = "Gdansk";
-  $scope.fullName = function(){
-    return $scope.firstName + " " + $scope.lastName;
-  };
-  //experience
-  $scope.startDate= 2014;
-  $scope.endDate;
-  $scope.companyName = "IKM";
-  $scope.positionName = "organizator wydarzeń";
-  $scope.positionDescription = "organizowanie eventów";
-  //experience
-  $scope.experience = [
-  {
-  "startDate" : 2013,
-  "endDate" : 2013,
-  "companyName": "Świetlica",
-  "positionName": "asystent",
-  "positionDescription" : "asystowanie"
-  },
-  {
-  "startDate" : 2013,
-  "endDate" : 2013,
-  "companyName": "Świetlica",
-  "positionName": "asystent",
-  "positionDescription" : "asystowanie"
-   }
-   ];
-  //education
-  $scope.schoolStartDate= 2010;
-  $scope.schoolEndDate= 2015;
-  $scope.schoolName = "Uniwersytet Gdański";
-  $scope.schoolSubject = "Kulturoznawstwo";
-  $scope.degree = "magister kulturoznawstwa";
-  //courses
-  $scope.courseYear = 2013;
-  $scope.courseOrganization = "Atena - Kursy i Szkolenia";
-  $scope.courseName = "Zarządzanie projektami";
-  $scope.courseDescription = "w organizacjach kultury i pozarządowych";
-  //language skills
-  $scope.foreignLanguage = "angielski";
-  $scope.languageLevel = "B2";
-  $scope.languages = ["angielski", "niemiecki", "hiszpański", "japoński", "rosyjski"];
-  //skills
-  $scope.skill = "obsługa pakietu Office";
-  //hobbies
-  $scope.hobby = "wyjazdy na safari, survival";
-});
-
-var pd_firstName = document.getElementById("pd_firstName").value;
-var pd_lastName = document.getElementById("pd_lastName").value;
-var pd_email = document.getElementById("pd_email").value;
-var pd_phone = document.getElementById("pd_phone").value;
-var pd_birthDate = document.getElementById("pd_birthDate").value;
-var pd_photo = document.getElementById("pd_photo").value;
-
-var ad_street = document.getElementById("ad_street").value;
-var ad_postalCode = document.getElementById("ad_postalCode").value;
-var ad_city = document.getElementById("ad_city").value;
-
-
 function createPersonalDataObject() {
+    var pd_firstName = document.getElementById("personalData-firstName").value;
+    var pd_lastName = document.getElementById("personalData-lastName").value;
+    var pd_email = document.getElementById("personalData-email").value;
+    var pd_phone = document.getElementById("personalData-phone").value;
+    var pd_birthDate = document.getElementById("personalData-birthDate").value;
+    var pd_photo = document.getElementById("personalData-photo").value;
+
     var personalData = {};
     personalData.firstName = pd_firstName;
     personalData.lastName = pd_lastName;
@@ -86,7 +23,11 @@ function createPersonalDataObject() {
     return personalData;
 }
 
-/*function createAddressObject() {
+function createAddressObject() {
+    var ad_street = document.getElementById("address-street").value;
+    var ad_postalCode = document.getElementById("address-postalCode").value;
+    var ad_city = document.getElementById("address-city").value;
+
     var address = {};
     address["street"] = ad_street;
     address["postalCode"] = ad_postalCode;
@@ -96,33 +37,105 @@ function createPersonalDataObject() {
 
 
 function createWorkExperiencesObject() {
+    var experiences_company_name = document.getElementById("experiences-company-name").value;
+    var experiences_company_city = document.getElementById("experiences-company-city").value;
+    var experiences_position = document.getElementById("experiences-position").value;
+    var experiences_description = document.getElementById("experiences-description").value;
+    var experiences_startDate = document.getElementById("experiences-startDate").value;
+    var experiences_endDate = document.getElementById("experiences-endDate").value;
+
+    var Experiences = [];
     var workExperiences = {};
     workExperiences.company = {};
-    workExperiences.company["name"] = ;
-    workExperiences.company["city"] = ;
-    workExperiences.position = "";
-    workExperiences.description = "";
-    workExperiences.startDate = "";
-    workExperiences.endDate = "";
+    workExperiences.company["name"] = experiences_company_name;
+    workExperiences.company["city"] = experiences_company_city;
+    workExperiences.position = experiences_position;
+    workExperiences.description = experiences_description;
+    workExperiences.startDate = experiences_startDate;
+    workExperiences.endDate = experiences_endDate;
     workExperiences.enabled = true;
-    return workExperiences;
+    Experiences.push(workExperiences);
+    return Experiences;
 }
 
 function createEducationsObject() {
-    var school = {};
-    var educations = [{}];
-}*/
+    var educations_school_name = document.getElementById("educations-school-name").value;
+    var educations_degree = document.getElementById("educations-degree").value;
+    var educations_startDate = document.getElementById("educations-startDate").value;
+    var educations_endDate = document.getElementById("educations-endDate").value;
+
+    var educationsArray = [];
+    var educations = {};
+    educations.school = {};
+    educations.school["name"] = educations_school_name;
+    educations.degree = educations_degree;
+    educations.startDate = educations_startDate;
+    educations.endDate = educations_endDate;
+    //educations.enabled = true;
+    educationsArray.push(educations);
+    return educationsArray;
+}
+
+function createLanguagesObject() {
+    var languages_name = document.getElementById("languages-name").value;
+    var languages_level = document.getElementById("languages-level").value;
+
+    var languagesArray = [];
+    var languages = {};
+    languages.name = languages_name;
+    languages.level = languages_level;
+    //languages.enabled = true;
+    languagesArray.push(languages);
+    return languagesArray;
+}
+
+function createHobbiesObject() {
+    var hobbies_name = document.getElementById("hobbies-name").value;
+
+    var hobbiesArray = [];
+    var hobbies = {};
+    hobbies.name = hobbies_name;
+    //hobbies.enabled = true;
+    hobbiesArray.push(hobbies);
+    return hobbiesArray;
+}
+
+function createSkillsObject() {
+    var skills_name = document.getElementById("skills-name").value;
+
+    var skillsArray = [];
+    var skills = {};
+    skills.name = skills_name;
+    //skills.enabled = true;
+    skillsArray.push(skills);
+    return skillsArray;
+}
+
+function createCoursesObject() {
+    var courses_name = document.getElementById("courses-name").value;
+    var courses_startDate = document.getElementById("courses-startDate").value;
+    var courses_endDate = document.getElementById("courses-endDate").value;
+
+    var coursesArray = [];
+    var courses = {};
+    courses.name = courses_name;
+    courses.startDate = courses_startDate;
+    courses.endDate = courses_endDate;
+    coursesArray.push(courses);
+    return coursesArray;
+
+}
 
 function createCVObject() {
     var completeCVObject = {};
     completeCVObject.personalData = createPersonalDataObject();
-    /*completeCVObject.address = createAddressObject();
+    completeCVObject.address = createAddressObject();
     completeCVObject.workExperiences = createWorkExperiencesObject();
-    completeCVObject.educations = createEducationsObjects();
-    completeCVObject.languages = createLanguages();
-    completeCVObject.hobbies = createHobbies();
-    completeCVObject.skills = createSkills();
-    completeCVObject.courses = createCourses();*/
+    completeCVObject.educations = createEducationsObject();
+    completeCVObject.languages = createLanguagesObject();
+    completeCVObject.hobbies = createHobbiesObject();
+    completeCVObject.skills = createSkillsObject();
+    completeCVObject.courses = createCoursesObject();
     return completeCVObject;
 }
 
@@ -146,9 +159,38 @@ function sendCurriculumData() {
                     window.location.href = "http://localhost:8080/register.html";
                 });*/
         })
-    /*   .catch(function(error) {
+    .catch(function(error) {
                // If there is any error you will catch them here
-       });*/
+        console.log(error);
+       })
 }
 
-document.getElementById("generateCV").addEventListener("click", sendCurriculumData);
+document.getElementById("sendCurriculumData").addEventListener("click", sendCurriculumData);
+
+function previewPersonalDataFirstName(value){
+    document.getElementById("preview-personalData-firstName").innerHTML = value;
+}
+function previewPersonalDataLastName(value){
+    document.getElementById("preview-personalData-lastName").innerHTML = value;
+}
+function previewPersonalDataEmail(value){
+    document.getElementById("preview-personalData-email").innerHTML = value;
+}
+function previewPersonalDataPhone(value){
+    document.getElementById("preview-personalData-phone").innerHTML = value;
+}
+function previewPersonalDataBirthDate(value){
+    document.getElementById("preview-personalData-birthDate").innerHTML = value;
+}
+function previewPersonalDataPhoto(value){
+    document.getElementById("preview-personalData-photo").innerHTML = value;
+}
+function previewAddressStreet(value){
+    document.getElementById("preview-address-street").innerHTML = value;
+}
+function previewAddressPostalCode(value){
+    document.getElementById("preview-address-postalCode").innerHTML = value;
+}
+function previewAddressCity(value){
+    document.getElementById("preview-address-city").innerHTML = value;
+}
